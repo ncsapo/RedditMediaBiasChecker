d04a62a4fe39acc4db60a5721f872fb8c29c0e53
ncsapo <nickcsapo@gmail.com>
2021-05-08 09:49:36 -0400

    4

--- nwebscraper.py
+++ nwebscraper.py
@@ -1,9 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
+
+#from bs4 import BeautifulSoup
 
 #Where are we getting this info?
 bias_checking_website = "https://mediabiasfactcheck.com/"
 
+def getBias(source_name):
+    page_data = requests.get(bias_checking_website + source_name).text
+    parts = page_data.split('data-image-title')
+
+    bias = parts[1]
+    bias = bias[2:7]
+
+    factual_reporting = parts[4]
+    factual_reporting = factual_reporting[6:19]
+
+    print(f"Bias: {bias}")
+    print(f"Factual Reporting: {factual_reporting}")
+    return bias, factual_reporting
+
+
+'''
 #Takes in the name of the media you want to fact-check. Needs to be identical to the bias_checking_website url to the source
 def getBias(sourceName):
     print("blah blah: ", sourceName)
@@ -24,5 +41,6 @@ def getBias(sourceName):
     return bias_title[:-2], factual_reporting_title[4:]
 
 #print(getBias('hot-air'))
+'''
 
 print("nwebscraper loaded")
