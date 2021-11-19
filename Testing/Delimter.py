e3849deb4556b00d5241e559a815faf187cae8bd
ThirdLaw <61213990+ThirdLaw@users.noreply.github.com>
2021-11-18 21:37:25 -0500

    Added link isolation
    Added link isolation

---
+++ Testing/Delimter.py
@@ -0,0 +1,4 @@
+TestContent = "asdkljfhasdlkjfhasd https://www.cnn.com/ askldhalskdjasldk sdkjafhsldkjf"
+
+print(("http" + TestContent.split("http")[1]).split(" ")[0])
+
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_message(message):
     if message.author == client.user and not message.author.bot:
         return
     if message.embeds:
-        response = MediaBias.check_bias(message.content)
+        response = MediaBias.check_bias(("http" + message.content.split("http")[1]).split(" ")[0]) # Cuts out everything before the http and everything after the first space after the link.
         if response == "The bot was unable to rate this source":
             await message.add_reaction("❌")
         else:
